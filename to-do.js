9d5750c32bb97b6a6fee549b63911d1796510107
Sharad Bista <bishalbista737@gmail.com>
2026-02-04 17:42:57 +0545

    feat:deleting and checking task

--- to-do.js
+++ to-do.js
@@ -1,24 +1,65 @@
-console.log("Script file is running — hello from Sharad's todo app");
-
-
 const inputTask = document.getElementById("to-do");
+
 const addButton = document.getElementById("submit");
-const taskList = document.getElementById("task-list")
 
-function addTask(){
-    let taskText = inputTask.value.trim();
+const taskList = document.getElementById("task-list");
 
-    if(taskText === ""){
-        alert("Write Something to add");
-        return;
+function addTask() {
+  let taskText = inputTask.value.trim();
+
+  if (taskText === "") {
+    alert("Write something to add in list");
+    return;
+  }
+
+  // creating list element
+  let li = document.createElement("li");
+
+  //checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  //textSpan
+  const textSpan = document.createElement("span");
+  textSpan.className = "task-text";
+  textSpan.textContent = taskText;
+
+  //deleteBtn
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "delete";
+  deleteBtn.className = "delete-btn";
+
+  // putting all together
+  li.appendChild(checkbox);
+  li.appendChild(textSpan);
+  li.appendChild(deleteBtn);
+
+  //add to taskList
+  taskList.appendChild(li);
+
+  inputTask.value = "";
+  inputTask.focus();
+
+  // toggle checkbox
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
+      li.classList.add("completed");
+    } else {
+      li.classList.remove("completed");
     }
-    let newItem = document.createElement("li"); 
-    newItem.textContent = taskText;
+  });
 
-    taskList.appendChild(newItem);
-
-    inputTask.value ="";
-    console.log("Add button was clicked! Input value was:", inputTask.value);
+  // delete button functionality
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+  });
 }
-
+// add task when click the button
 addButton.addEventListener("click", addTask);
+
+// allow enter to input task
+inputTask.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
